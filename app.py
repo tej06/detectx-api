73c96e641a1014821b3076308a4e3f02259ad374
Tejas Udayakumar <tejasu.kumar123@gmail.com>
2019-10-07 23:11:48 +0530

    Add files via upload
    Added app elements

---
+++ app.py
@@ -0,0 +1,70 @@
+import os
+import numpy as np
+import json
+from flask import Flask, request, jsonify, make_response
+import tensorflow as tf
+from keras.models import load_model, model_from_json
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input
+from werkzeug.utils import secure_filename
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='../dist/static')
+CORS(app)
+
+# MODEL_PATH = 'models/detectx-39.hdf5'
+MODEL_JSON_PATH = 'models/model.json'
+graph = tf.get_default_graph()
+loaded_json_model = None
+with open(MODEL_JSON_PATH) as json_model:
+	loaded_model_json = json_model.read()
+# model = load_model(MODEL_PATH)
+model = model_from_json(loaded_model_json)
+# print(model.summary())
+
+def predict(img_file):
+	img = image.load_img(img_file, target_size=(150,150))
+	x = image.img_to_array(img)
+	x = np.expand_dims(x, axis=0)
+	# print("X", x.shape)
+	x = preprocess_input(x, mode='keras')
+	prediction = None
+	with graph.as_default():
+		prediction = model.predict(x)
+		# print("Prediction", prediction)
+	pred_class = np.argmax(prediction[0])
+	label = "Not Dangerous" if pred_class==1 else "Dangerous"
+	detectDanger = False if pred_class==1 else True
+	score = prediction[0][pred_class] * 100
+	return label, score, detectDanger
+
+@app.route("/classify", methods=["GET","POST"])
+def classify():
+	if request.method == "OPTIONS": # CORS preflight
+	    return _build_cors_prelight_response()
+	elif request.method == "POST":
+		# print("Input", request.files)
+		img = request.files['image']
+		basepath = os.path.dirname(__file__)
+		img_path = os.path.join(basepath, 'uploads', secure_filename(img.filename))
+		img.save(img_path)
+		label, score, detectDanger = predict(img_path)
+		result = [
+        	{
+                'label': label,
+                'score': score,
+				'detectDanger': detectDanger
+        	}
+		]
+		return jsonify(result)
+	return None
+
+def _build_cors_prelight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+if __name__ == "__main__":
+  app.run()
